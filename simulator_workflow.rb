58132060e7166678157f826aa4a1f96dfe014b0f
yamatatsu10969 <52.wadachi.96@gmail.com>
2024-02-27 21:34:32 -0800

    Update

--- simulator_workflow.rb
+++ simulator_workflow.rb
@@ -14,16 +14,23 @@ class SimulatorWorkflow
   def self.create_script_filter_items(devices)
     ios_simulator_keys = devices.keys.select { |key| key.to_s.include?('iOS') }
     ios_simulators = []
+    iphone_simulators = []
+    ipad_simulators = []
     ios_simulator_keys.each do |key|
-      devices[key].each do |device|
+      devices[key].reverse_each do |device|
         next unless device[:isAvailable] == true
 
         ios_version = key.to_s.split('iOS')[1]
         device[:name] = device[:name] + " (iOS #{ios_version})"
-        ios_simulators << Simulator.new(device).to_script_filter_item
+        if device[:name].include?('iPad')
+          ipad_simulators << Simulator.new(device).to_script_filter_item
+        else
+          iphone_simulators << Simulator.new(device).to_script_filter_item
+        end
       end
     end
-    ios_simulators
+    ios_simulators.concat(iphone_simulators).concat(ipad_simulators)
+
   end
 
   def self.show_simulators
