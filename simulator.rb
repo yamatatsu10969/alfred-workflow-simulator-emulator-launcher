253f62a2adaa18658ba391742c26444c08e621c3
yamatatsu10969 <52.wadachi.96@gmail.com>
2025-03-20 22:09:58 +0900

    Restore

---
+++ simulator.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative 'script_filter_item'
+
+# iOS Simulator
+class Simulator
+  attr_accessor :name, :udid
+
+  def initialize(args = {})
+    @name = args[:name]
+    @udid = args[:udid]
+  end
+
+  def to_script_filter_item
+    ScriptFilterItem.new(
+      { arg: @udid,
+        title: @name,
+        icon_path: 'assets/simulator.png' }
+    )
+  end
+end
