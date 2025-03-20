dab57f0f372a9e795af7616dfcb4ce048be349ac
yamatatsu10969 <52.wadachi.96@gmail.com>
2025-03-20 22:02:09 +0900

    Remove logs

--- script_filter_item.rb
+++ script_filter_item.rb
@@ -4,39 +4,31 @@ require 'json'
 
 # Item shown in the script filter
 class ScriptFilterItem
-  attr_accessor :title, :arg, :icon_path
+  attr_reader :uid, :title, :subtitle, :arg, :icon, :mods
 
-  def initialize(args = {})
-    @title = args[:title]
-    @arg = args[:arg]
-    @icon_path = args[:icon_path]
-
-    # Create a debug log
-    log_file = '/tmp/alfred_script_filter_items.log'
-    File.write(log_file, "Created ScriptFilterItem: title=#{@title}, arg=#{@arg}\n", mode: 'a')
+  def initialize(options = {})
+    @uid = options[:uid]
+    @title = options[:title]
+    @subtitle = options[:subtitle]
+    @arg = options[:arg]
+    @icon = options[:icon]
+    @mods = options[:mods] || {}
   end
 
   def to_json(*)
     to_h.to_json
   end
 
-  # Add a to_h method to convert to hash
+  # Convert to hash for JSON output
   def to_h
-    item_hash = {
-      type: 'file',
-      title: title,
-      subtitle: '',
-      arg: arg,
-      icon: {
-        path: icon_path
-      }
+    {
+      uid: @uid,
+      title: @title,
+      subtitle: @subtitle,
+      arg: @arg,
+      icon: @icon,
+      mods: @mods
     }
-
-    # Create a debug log
-    log_file = '/tmp/alfred_script_filter_items.log'
-    File.write(log_file, "Converting to hash: #{item_hash.inspect}\n", mode: 'a')
-
-    item_hash
   end
 
   # For inspection
--- simulator_workflow.rb
+++ simulator_workflow.rb
@@ -13,13 +13,7 @@ class SimulatorWorkflow
   end
 
   def self.create_script_filter_items(devices)
-    # Create debug log
-    log_file = '/tmp/alfred_simulator_workflow.log'
-    File.write(log_file, "==== Simulator Workflow Started: #{Time.now} ====\n")
-    File.write(log_file, "Current directory: #{Dir.pwd}\n", mode: 'a')
-
     ios_simulator_keys = devices.keys.select { |key| key.to_s.include?('iOS') }
-    File.write(log_file, "Found iOS keys: #{ios_simulator_keys.inspect}\n", mode: 'a')
 
     ios_simulators = []
     iphone_simulators = []
@@ -33,7 +27,6 @@ class SimulatorWorkflow
         device[:name] = device[:name] + " (iOS #{ios_version})"
 
         simulator_item = Simulator.new(device).to_script_filter_item
-        File.write(log_file, "Created item: #{simulator_item.inspect}, udid: #{device[:udid]}\n", mode: 'a')
 
         if device[:name].include?('iPad')
           ipad_simulators << simulator_item
@@ -43,76 +36,30 @@ class SimulatorWorkflow
       end
     end
 
-    File.write(log_file, "Total iPhone simulators: #{iphone_simulators.size}\n", mode: 'a')
-    File.write(log_file, "Total iPad simulators: #{ipad_simulators.size}\n", mode: 'a')
-
     all_simulators = iphone_simulators + ipad_simulators
 
-    # Log unsorted simulators
-    File.write(log_file, "==== UNSORTED SIMULATORS ====\n", mode: 'a')
-    all_simulators.each_with_index do |sim, index|
-      File.write(log_file, "[#{index}] #{sim.title} (#{sim.arg})\n", mode: 'a')
-    end
-
     begin
-      # Load history for debugging
-      history = DeviceHistory.load_history
-      File.write(log_file, "==== HISTORY DATA ====\n", mode: 'a')
-      history.each do |key, value|
-        File.write(log_file, "#{key}: #{value.inspect}\n", mode: 'a')
-      end
-
       # Sort by history using DeviceHistory class
-      File.write(log_file, "Sorting by history...\n", mode: 'a')
-      sorted_simulators = DeviceHistory.sort_by_history(all_simulators, 'simulator')
-      File.write(log_file, "Sorting complete. Simulators count: #{sorted_simulators.size}\n", mode: 'a')
-
-      # Log sorted simulators
-      File.write(log_file, "==== SORTED SIMULATORS ====\n", mode: 'a')
-      sorted_simulators.each_with_index do |sim, index|
-        File.write(log_file, "[#{index}] #{sim.title} (#{sim.arg})\n", mode: 'a')
-      end
-
-      sorted_simulators
+      DeviceHistory.sort_by_history(all_simulators, 'simulator')
     rescue => e
-      # Log error and return unsorted simulators as fallback
-      File.write(log_file, "ERROR during sorting: #{e.class}: #{e.message}\n", mode: 'a')
-      File.write(log_file, "Backtrace: #{e.backtrace.join("\n")}\n", mode: 'a')
-      File.write(log_file, "Returning unsorted simulators\n", mode: 'a')
+      # Return unsorted simulators as fallback
       all_simulators
     end
   end
 
   def self.show_simulators
     begin
-      # Create debug log
-      log_file = '/tmp/alfred_simulator_workflow.log'
-      File.write(log_file, "Getting device list...\n", mode: 'a')
-
       items = create_script_filter_items(device_list)
-      File.write(log_file, "Converting to hashes...\n", mode: 'a')
 
       # Convert ScriptFilterItem objects to hashes
       item_hashes = items.map(&:to_h)
-      File.write(log_file, "Created #{item_hashes.size} item hashes\n", mode: 'a')
-
-      # Log JSON item order
-      File.write(log_file, "==== FINAL JSON ITEMS ORDER ====\n", mode: 'a')
-      item_hashes.each_with_index do |item, index|
-        File.write(log_file, "[#{index}] #{item[:title]} (#{item[:arg]})\n", mode: 'a')
-      end
 
       export_json = {
         'items' => item_hashes
       }.to_json
 
-      File.write(log_file, "Exporting JSON with #{item_hashes.size} items\n", mode: 'a')
       puts export_json
-      File.write(log_file, "Workflow completed successfully\n", mode: 'a')
     rescue => e
-      # Log error and return empty list as fallback
-      File.write(log_file, "ERROR: #{e.class}: #{e.message}\n", mode: 'a')
-      File.write(log_file, "Backtrace: #{e.backtrace.join("\n")}\n", mode: 'a')
       puts '{"items": []}'
     end
   end
